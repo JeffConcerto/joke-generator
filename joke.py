697af5698e76e7564df91e7d4ae5935301f255fe
Jeffrey <anappconcerto@gmail.com>
2025-07-13 00:17:17 -0400

    Inital commit

---
+++ joke.py
@@ -0,0 +1,12 @@
+import random
+
+joke_list = [
+        "Why do programmers prefer dark mode? Because light attracts bugs.",
+        "I've got a great UDP joke, but I'm not sure you'll get it.",
+        "There are only 10 types of people in the world: those who understand binary, and those who don't.",
+        "A programmer's wife says, 'Go to the store and buy a loaf of bread. If they have eggs, buy a dozen.' The programmer returns with 12 loaves of bread.",
+        "What's the object-oriented way to become wealthy? Inheritance.",
+        ]
+
+print(random.choice(joke_list))
+
